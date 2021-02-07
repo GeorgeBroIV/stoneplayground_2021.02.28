7a3b3422f1658246c165b9a2509ffa59f883b720
George Brotherston <George@StonePlayground.com>
2021-02-07 02:20:14 -0500

    Expanded User model Refactored Vue components

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -8,9 +8,19 @@
 
         <form @submit.prevent="submit">
             <div>
-                <jet-label for="name" value="Name" />
-                <jet-input id="name" type="text" class="mt-1 block w-full" v-model="form.name" required autofocus autocomplete="name" />
+                <jet-label for="firstname" value="First Name" />
+                <jet-input id="firstname" type="text" class="mt-1 block w-full" v-model="form.firstname" required autofocus autocomplete="firstname" />
             </div>
+
+          <div class="mt-4">
+            <jet-label for="lastname" value="Last Name" />
+            <jet-input id="lastname" type="text" class="mt-1 block w-full" v-model="form.lastname" required autocomplete="lastname" />
+          </div>
+
+          <div class="mt-4">
+            <jet-label for="username" value="User Name" />
+            <jet-input id="username" type="text" class="mt-1 block w-full" v-model="form.username" required autocomplete="username" />
+          </div>
 
             <div class="mt-4">
                 <jet-label for="email" value="Email" />
@@ -75,7 +85,9 @@
         data() {
             return {
                 form: this.$inertia.form({
-                    name: '',
+                    firstname: '',
+                    lastname: '',
+                    username: '',
                     email: '',
                     password: '',
                     password_confirmation: '',
--- resources/js/Pages/Profile/UpdateProfileInformationForm.vue
+++ resources/js/Pages/Profile/UpdateProfileInformationForm.vue
@@ -20,7 +20,7 @@
 
                 <!-- Current Profile Photo -->
                 <div class="mt-2" v-show="! photoPreview">
-                    <img :src="user.profile_photo_url" :alt="user.name" class="rounded-full h-20 w-20 object-cover">
+                    <img :src="user.profile_photo_url" :alt="user.firstname" class="rounded-full h-20 w-20 object-cover">
                 </div>
 
                 <!-- New Profile Photo Preview -->
@@ -41,11 +41,25 @@
                 <jet-input-error :message="form.errors.photo" class="mt-2" />
             </div>
 
-            <!-- Name -->
+            <!-- First Name -->
             <div class="col-span-6 sm:col-span-4">
-                <jet-label for="name" value="Name" />
-                <jet-input id="name" type="text" class="mt-1 block w-full" v-model="form.name" autocomplete="name" />
-                <jet-input-error :message="form.errors.name" class="mt-2" />
+              <jet-label for="firstname" value="First Name" />
+              <jet-input id="firstname" type="text" class="mt-1 block w-full" v-model="form.firstname" autocomplete="firstname" />
+              <jet-input-error :message="form.errors.firstname" class="mt-2" />
+            </div>
+
+            <!-- Last Name -->
+            <div class="col-span-6 sm:col-span-4">
+                <jet-label for="lastname" value="Last Name" />
+                <jet-input id="lastname" type="text" class="mt-1 block w-full" v-model="form.lastname" autocomplete="lastname" />
+                <jet-input-error :message="form.errors.lastname" class="mt-2" />
+            </div>
+
+            <!-- Username -->
+            <div class="col-span-6 sm:col-span-4">
+              <jet-label for="username" value="Username" />
+              <jet-input id="username" type="text" class="mt-1 block w-full" v-model="form.username" autocomplete="username" />
+              <jet-input-error :message="form.errors.username" class="mt-2" />
             </div>
 
             <!-- Email -->
@@ -94,7 +108,9 @@
             return {
                 form: this.$inertia.form({
                     _method: 'PUT',
-                    name: this.user.name,
+                    firstname: this.user.firstname,
+                    lastname: this.user.lastname,
+                    username: this.user.username,
                     email: this.user.email,
                     photo: null,
                 }),
