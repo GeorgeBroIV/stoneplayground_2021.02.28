8f6aac4206c21d3149576c49ac39f6271ef16206
George Brotherston <George@StonePlayground.com>
2021-02-19 07:39:17 -0500

    Refactored Trivia load errors

--- resources/js/Pages/Trivia.vue
+++ resources/js/Pages/Trivia.vue
@@ -1,161 +1,160 @@
 <!-- Tutorial for Vue Trivia: https://www.youtube.com/watch?v=4deVCNJq3qc&list=WL&index=33 -->
 <template>
 	<app-layout>
-		<!-- Trivia Header -->
-		<div align="center">
-			<b-nav tabs>
-				<b-nav-item disabled><b>Trivia</b></b-nav-item>
-				<b-nav-item v-if="this.triviaActive" disabled class="align-middle">Question {{ numCurrentQuestionDisplay }} of {{ this.numTotalQuestions }}.  Correct answers: {{ numCorrect }}.</b-nav-item>
-			</b-nav>
-		</div>
+		<div>
+			<!-- Trivia Header -->
+			<div align="center">
+				<b-nav tabs>
+					<b-nav-item disabled><b>Trivia</b></b-nav-item>
+					<b-nav-item v-if="this.triviaActive" disabled class="align-middle">Question {{ numCurrentQuestionDisplay }} of {{ this.numTotalQuestions }}.  Correct answers: {{ numCorrect }}.</b-nav-item>
+				</b-nav>
+			</div>
 
-		<!-- Trivia Content -->
-		<div class="py-3">
-			<b-container class="bv-example-row">
-				<b-row class="justify-content-md-center">
-					<!-- Trivia Stats -->
-					<b-col sm="4">
-						<div class="h3">
-							Developer Notes
-						</div>
-						<div class="mt-3 mb-1 font-bold">
-							Now Operational
-						</div>
-						<div class="mt-2 text-gray-500">
-							A cool Trivia game!
-						</div>
-						<div class="mt-3 font-bold">
-							Soon to come
-						</div>
-						<div class="mt-2 text-gray-500">
-							Ensure fresh questions between sessions
-						</div>
-						<div class="mt-2 text-gray-500">
-							Trivia Stats
-						</div>
-						<div class="mt-2 text-gray-500">
-							Persistent tab navigation where navigating between pages won't lose your current game state.
-						</div>
-					</b-col>
+			<!-- Trivia Content -->
+			<div class="py-3">
+				<b-container class="bv-example-row">
+					<b-row class="justify-content-md-center">
+						<!-- Trivia Stats -->
+						<b-col sm="4">
+							<div class="h3">
+								Developer Notes
+							</div>
+							<div class="mt-3 mb-1 font-bold">
+								Now Operational
+							</div>
+							<div class="mt-2 text-gray-500">
+								A cool Trivia game!
+							</div>
+							<div class="mt-3 font-bold">
+								Soon to come
+							</div>
+							<div class="mt-2 text-gray-500">
+								Ensure fresh questions between sessions
+							</div>
+							<div class="mt-2 text-gray-500">
+								Trivia Stats
+							</div>
+							<div class="mt-2 text-gray-500">
+								Persistent tab navigation where navigating between pages won't lose your current game state.
+							</div>
+						</b-col>
 
-					<!-- Trivia Options / Board -->
-					<b-col sm="8">
-						<!-- Trivia Options -->
-						<div id="triviaOptions" v-if="!triviaActive">
+						<!-- Trivia Options / Board -->
+						<b-col sm="8">
+							<!-- Trivia Options -->
+							<div id="triviaOptions" v-if="!triviaActive">
 
-							<!-- START - Content from TriviaOptions.vue (commented out below) -->
-							<div>
-								<div class="h3 mb-3">
-									Trivia Game Options
-								</div>
+								<!-- START - Content from TriviaOptions.vue (commented out below) -->
 								<div>
-									Trivia Category
-								</div>
-								<select v-model="selectedCategory" class="mb-3">
-									<option
-										v-for="(category, index) in this.triviaCategories"
-										:value="index">
-										{{ category }}
-									</option>
-								</select>
-
-								<div>
-									<div>
-										Criteria: Difficulty
+									<div class="h3 mb-3">
+										Trivia Game Options
 									</div>
-									<select v-model="selectedDifficulty" class="mb-3">
+									<div>
+										Trivia Category
+									</div>
+									<select v-model="selectedCategory" class="mb-3">
 										<option
-											v-for="(difficulty, index) in this.triviaDifficulties"
+											v-for="(category, index) in this.triviaCategories"
 											:value="index">
-											{{ difficulty }}
+											{{ category }}
 										</option>
 									</select>
-								</div>
 
-								<div>
 									<div>
-										Criteria: Answer Type
+										<div>
+											Criteria: Difficulty
+										</div>
+										<select v-model="selectedDifficulty" class="mb-3">
+											<option
+												v-for="(difficulty, index) in this.triviaDifficulties"
+												:value="index">
+												{{ difficulty }}
+											</option>
+										</select>
 									</div>
-									<select v-model="selectedType" class="mb-3">
-										<option
-											v-for="(type, index) in this.triviaTypes"
-											:value="index">
-											{{ type }}
-										</option>
-									</select>
-								</div>
 
-								<div>
 									<div>
-										Criteria: Questions
+										<div>
+											Criteria: Answer Type
+										</div>
+										<select v-model="selectedType" class="mb-3">
+											<option
+												v-for="(type, index) in this.triviaTypes"
+												:value="index">
+												{{ type }}
+											</option>
+										</select>
 									</div>
-									<b-select v-model="selectedAmount" class="mb-3 w-auto">
-										<option v-for="amount in this.amtMax">
-											{{ amount }}
-										</option>
-									</b-select>
-								</div>
 
-								<div align="center" v-if="this.empty && !this.loadFailed" class="mt-0 mb-3 py-2 text-gray-700 bg-blue-100 w-50">
-									<div class="mb-2 h5">
-										Loading...
-									</div>
-								</div>
-
-								<div align="center" v-if="this.loadFailed" class="mt-0 mb-3 py-2 text-gray-700 bg-blue-100 w-50">
-									<div class="mb-2 h5">
-										Sorry, unable to load your selection.
-									</div>
 									<div>
-										Try adjusting the Criteria (e.g. less Questions)
+										<div>
+											Criteria: Questions
+										</div>
+										<b-select v-model="selectedAmount" class="mb-3 w-auto">
+											<option v-for="amount in this.amtMax">
+												{{ amount }}
+											</option>
+										</b-select>
+									</div>
+
+									<div align="center" v-if="this.empty && !this.didloadFail && !this.bNewTrivia" class="mt-0 mb-3 py-2 text-gray-700 bg-blue-100 w-50">
+										<div class="mb-2 h5">
+											Loading...
+										</div>
+									</div>
+
+									<div align="center" v-if="this.didloadFail" class="mt-0 mb-3 py-2 text-gray-700 bg-blue-100 w-50">
+										<div class="mb-2 h5">
+											Sorry, unable to load your selection.
+										</div>
+										<div>
+											Try adjusting the Criteria (e.g. less Questions)
+										</div>
+									</div>
+
+									<div class="mt-3">
+										<b-button v-if="(!this.triviaActive) && (!this.empty || this.didloadFail)" @click="startSurvey" variant="primary">
+											Start Trivia
+										</b-button>
 									</div>
 								</div>
+								<!-- END - Content from TriviaOptions.vue (commented out below) -->
 
-								<div class="mt-3">
-									<b-button v-if="!this.empty || this.loadFailed" @click="startSurvey" variant="primary">
-										Start Trivia
+
+								<!--	              <TriviaOptions
+													:numTotalQuestions="numTotalQuestions"
+													:triviaCategories="triviaCategories"
+													:triviaDifficulties="triviaDifficulties"
+													:triviaTypes="triviaTypes"
+													:questions="questions"
+													:triviaActive="triviaActive"
+													:loading="loading"
+												  /> -->
+							</div>
+							<!-- Trivia Board -->
+							<div id="triviaBoard" v-if="triviaActive">
+								<!-- Wait until server response before passing data by ensuring questions.length is not zero -->
+								<TriviaContent
+									v-if="questions.length"
+									:currentQuestion="questions[index]"
+									:next="next"
+									:increment="increment"
+									:numCurrentQuestion="numCurrentQuestion"
+									:numTotalQuestions="numTotalQuestions"
+									:newTrivia="newTrivia"
+								/>
+								<div align="center" v-if="numCurrentQuestion-1 === numTotalQuestions && this.triviaActive">
+									<b-button variant="primary" @click="newTrivia">
+										New Trivia Game
 									</b-button>
 								</div>
 							</div>
-							<!-- END - Content from TriviaOptions.vue (commented out below) -->
 
-
-							<!--	              <TriviaOptions
-												:numTotalQuestions="numTotalQuestions"
-												:triviaCategories="triviaCategories"
-												:triviaDifficulties="triviaDifficulties"
-												:triviaTypes="triviaTypes"
-												:questions="questions"
-												:triviaActive="triviaActive"
-												:loading="loading"
-											  /> -->
-						</div>
-						<!-- Trivia Board -->
-						<div id="triviaBoard" v-if="triviaActive">
-							<!-- Wait until server response before passing data by ensuring questions.length is not zero -->
-							<TriviaContent
-								v-if="questions.length"
-								:currentQuestion="questions[index]"
-								:next="next"
-								:increment="increment"
-								:numCurrentQuestion="numCurrentQuestion"
-								:numTotalQuestions="numTotalQuestions"
-								:triviaActive="triviaActive"
-								:newTrivia="newTrivia"
-							/>
-							<div align="center" v-if="numCurrentQuestion-1 === numTotalQuestions">
-								<b-button variant="primary" @click="newTrivia">
-									New Trivia Game
-								</b-button>
-							</div>
-						</div>
-
-					</b-col>
-				</b-row>
-			</b-container>
-
+						</b-col>
+					</b-row>
+				</b-container>
+			</div>
 		</div>
-
 	</app-layout>
 </template>
 
@@ -201,7 +200,8 @@ export default {
 			selectedAmount: 10,
 			amtMax: 50,
 			empty: false,
-			loadFailed: false,
+			didloadFail: false,
+			bNewTrivia: false,
 		}
 	},
 	watch: {
@@ -210,8 +210,14 @@ export default {
 				let myVar = setTimeout(this.loadFailed, 3000);
 				if(!this.questions.length) {
 					this.empty = true
-					this.loadFailed = true
+					this.didloadFail = true
 				}
+			}
+		},
+		triviaActive: {
+			immediate: true,
+			handler() {
+				this.triviaActive = this.triviaActive;
 			}
 		}
 	},
@@ -219,7 +225,8 @@ export default {
 		loadFailed() {
 			if(this.questions.length) {
 				this.empty = false
-				this.triviaActive = !this.triviaActive
+				this.triviaActive = true
+				this.bNewTrivia = false
 			}
 		},
 		next() {
@@ -233,6 +240,8 @@ export default {
 			this.numCurrentQuestion++
 		},
 		newTrivia() {
+			this.bNewTrivia = true;
+			this.triviaActive = false;
 			this.selectedCategory = 0;
 			this.selectedDifficulty = "any";
 			this.selectedType = "any";
@@ -244,10 +253,9 @@ export default {
 			this.numCurrentQuestionDisplay = 1;
 			this.numTotalQuestions = 0;
 			this.selectedIndex = 0;
-			this.startSurvey();
-			this.loadFailed = false;
 		},
 		startSurvey() {
+			this.didloadFail = false;
 			let urlCategory = this.selectedCategory === '0' ? '' : '&category=' + this.selectedCategory;
 			let urlDifficulty = this.selectedDifficulty === 'any' ? '' : '&difficulty=' + this.selectedDifficulty;
 			let urlType = this.selectedType === 'any' ? '' : '&type=' + this.selectedType;
@@ -265,7 +273,7 @@ export default {
 			let loaded=this.callFetch(urlTrivia);
 			if(this.questions) {
 				this.empty = false
-				this.triviaActive = !this.triviaActive
+				this.triviaActive = true
 			} else {
 				this.empty = true
 			}
